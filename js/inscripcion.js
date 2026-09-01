9a03c0f72eab45ffe75843d1fb0851a97a70e18d
sebai <sebaignaciokr20@gmail.com>
2026-08-31 23:58:04 -0400

    se valida jugador y equipo en inscripcion

--- js/inscripcion.js
+++ js/inscripcion.js
@@ -109,7 +109,7 @@ formularioInscripcion.addEventListener("submit", function (event) {
         return;
     }
 
-    let participante;
+   let participante;
 
     if (tipoParticipante.value === "equipo") {
 
@@ -144,40 +144,3 @@ formularioInscripcion.addEventListener("submit", function (event) {
 
         participante = usuarioActual.apodo;
     }
-
-    const yaInscrito = inscripciones.some(function (inscripcion) {
-        return (
-            inscripcion.torneo === torneoSeleccionado.nombre &&
-            inscripcion.participante === participante
-        );
-    });
-
-    if (yaInscrito) {
-
-        if (tipoParticipante.value === "equipo") {
-            mensajeInscripcion.textContent =
-                "Este equipo ya se encuentra inscrito en este torneo.";
-        } else {
-            mensajeInscripcion.textContent =
-                "Este jugador ya se encuentra inscrito en este torneo.";
-        }
-
-        return;
-    }
-
-    inscripciones.push({
-        torneo: torneoSeleccionado.nombre,
-        participante: participante,
-        tipo: tipoParticipante.value
-    });
-
-    mensajeInscripcion.textContent = "";
-
-    resumenInscripcion.innerHTML = `
-    <h3>Inscripción realizada correctamente</h3>
-    <p>Torneo: ${torneoSeleccionado.nombre}</p>
-    <p>Participante: ${participante}</p>
-    <p>Modalidad: ${torneoSeleccionado.modalidad}</p>
-    `;
-
-});
