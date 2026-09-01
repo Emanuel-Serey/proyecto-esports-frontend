c645cb72bb2db7da28cf46f02666a3df187e7acb
Emanuel Serey <em.serey@duocuc.cl>
2026-08-31 22:09:41 -0400

    Se agrega datos simulados de torneos

---
+++ js/datos.js
@@ -0,0 +1,34 @@
+const torneos = [
+    {
+        nombre: "Arena Masters",
+        juego: "Valorant",
+        modalidad: "Por equipos",
+        integrantesPorEquipo: 5,
+        estado: "Abierto",
+        cupoMaximo: 16,
+        inscritos: 10,
+        cierreInscripcion: "2026-09-05"
+    },
+
+    {
+        nombre: "Legends Cup",
+        juego: "League of Legends",
+        modalidad: "Por equipos",
+        integrantesPorEquipo: 5,
+        estado: "En curso",
+        cupoMaximo: 4,
+        inscritos: 4,
+        cierreInscripcion: "2026-09-10"
+    },
+
+    {
+        nombre: "Rocket Championship",
+        juego: "Rocket League",
+        modalidad: "Individual",
+        integrantesPorEquipo: 1,
+        estado: "Abierto",
+        cupoMaximo: 12,
+        inscritos: 8,
+        cierreInscripcion: "2026-09-25"
+    }
+];
