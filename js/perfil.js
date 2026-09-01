5a642856124d5ca55ba42a17079a860b8f1e72e9
xino <benja.gutierreza@duocuc.cl>
2026-09-01 00:41:31 -0400

    Se agregan mostrar historial, estadisticas y sanciones

--- js/perfil.js
+++ js/perfil.js
@@ -41,3 +41,45 @@ perfilJugador.equipos.forEach(function (equipo) {
     `;
 
 });
+
+// HISTORIAL DE TORNEOS
+
+perfilJugador.historialTorneos.forEach(function (torneo) {
+
+    listaHistorial.innerHTML += `
+        <article class="tarjeta-torneo">
+            <h3>${torneo.torneo}</h3>
+            <p>Juego: ${torneo.juego}</p>
+            <p>Resultado: ${torneo.resultado}</p>
+        </article>
+    `;
+
+});
+
+
+// ESTADÍSTICAS
+
+datosEstadisticas.innerHTML = `
+    <p>Victorias: ${perfilJugador.estadisticas.victorias}</p>
+    <p>Derrotas: ${perfilJugador.estadisticas.derrotas}</p>
+`;
+
+
+// SANCIONES
+
+if (perfilJugador.sanciones.length === 0) {
+
+    listaSanciones.innerHTML =
+        "<p>Sin sanciones vigentes.</p>";
+
+} else {
+
+    perfilJugador.sanciones.forEach(function (sancion) {
+
+        listaSanciones.innerHTML += `
+            <p>${sancion}</p>
+        `;
+
+    });
+
+}
