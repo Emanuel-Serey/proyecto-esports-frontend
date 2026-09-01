8f3bac4a974d3303393724bff1b9a5313a93c934
sebai <sebaignaciokr20@gmail.com>
2026-08-31 23:29:06 -0400

    se agregan datos simulados de detalle-torneo

--- js/datos.js
+++ js/datos.js
@@ -32,3 +32,128 @@ const torneos = [
         cierreInscripcion: "2026-09-25"
     }
 ];
+
+const detalleTorneo = {
+    nombre: "Legends Cup",
+    juego: "League of Legends",
+    modalidad: "Por equipos",
+    estado: "En curso",
+    formato: "BO3",
+    integrantesPorEquipo: 5,
+    cupoMaximo: 4,
+    inscritos: 4,
+    cierreInscripcion: "2026-09-10",
+
+    participantes: [
+        "T1",
+        "Gen.G",
+        "G2 Esports",
+        "Bilibili Gaming"
+    ],
+
+    partidas: [
+        {
+            ronda: "Jornada 1",
+            equipo1: "T1",
+            equipo2: "G2 Esports",
+            horario: "2026-09-15 18:00",
+            estado: "Finalizada",
+            resultado: "2 - 0"
+        },
+        {
+            ronda: "Jornada 1",
+            equipo1: "Gen.G",
+            equipo2: "Bilibili Gaming",
+            horario: "2026-09-15 20:00",
+            estado: "Finalizada",
+            resultado: "2 - 1"
+        },
+        {
+            ronda: "Jornada 2",
+            equipo1: "Gen.G",
+            equipo2: "T1",
+            horario: "2026-09-18 18:00",
+            estado: "Finalizada",
+            resultado: "2 - 1"
+        },
+        {
+            ronda: "Jornada 2",
+            equipo1: "Bilibili Gaming",
+            equipo2: "G2 Esports",
+            horario: "2026-09-18 20:00",
+            estado: "Finalizada",
+            resultado: "2 - 0"
+        },
+        {
+            ronda: "Jornada 3",
+            equipo1: "T1",
+            equipo2: "Bilibili Gaming",
+            horario: "2026-09-21 18:00",
+            estado: "Programada",
+            resultado: "-"
+        },
+        {
+            ronda: "Jornada 3",
+            equipo1: "Gen.G",
+            equipo2: "G2 Esports",
+            horario: "2026-09-21 20:00",
+            estado: "Programada",
+            resultado: "-"
+        }
+    ],
+
+    clasificacion: [
+        {
+            posicion: 1,
+            equipo: "Gen.G",
+            jugados: 2,
+            ganados: 2,
+            perdidos: 0,
+            diferenciaMapas: 2,
+            puntos: 6
+        },
+        {
+            posicion: 2,
+            equipo: "T1",
+            jugados: 2,
+            ganados: 1,
+            perdidos: 1,
+            diferenciaMapas: 1,
+            puntos: 3
+        },
+        {
+            posicion: 3,
+            equipo: "Bilibili Gaming",
+            jugados: 2,
+            ganados: 1,
+            perdidos: 1,
+            diferenciaMapas: 1,
+            puntos: 3
+        },
+        {
+            posicion: 4,
+            equipo: "G2 Esports",
+            jugados: 2,
+            ganados: 0,
+            perdidos: 2,
+            diferenciaMapas: -4,
+            puntos: 0
+        }
+    ],
+
+    premios: [
+        {
+            posicion: 1,
+            premio: "$500.000"
+        },
+        {
+            posicion: 2,
+            premio: "$250.000"
+        },
+        {
+            posicion: 3,
+            premio: "$100.000"
+        }
+    ]
+};
+
